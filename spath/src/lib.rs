1e5cf741fcb07ba7c558a0418b73a5712ee2baa0
tison <wander4096@gmail.com>
2025-01-07 00:17:59 +0800

    fixup
    Signed-off-by: tison <wander4096@gmail.com>

--- spath/src/lib.rs
+++ spath/src/lib.rs
@@ -26,7 +26,7 @@
 //! # #[cfg(feature = "json")]
 //! # {
 //! use serde_json::json;
-//! use serde_json::Value as JsonValue;
+//! use serde_json::Value;
 //! use spath::SPath;
 //! use spath::VariantValue;
 //!
@@ -40,9 +40,8 @@
 //! });
 //!
 //! let spath = SPath::new("$.phones[1]").unwrap();
-//! let value = VariantValue::from(data);
-//! let result = spath.eval(&value).unwrap();
-//! assert_eq!(JsonValue::from(result), json!("+44 2345678"));
+//! let result = spath.eval(&data).unwrap();
+//! assert_eq!(result, json!("+44 2345678"));
 //! # }
 //! ```
 //!
